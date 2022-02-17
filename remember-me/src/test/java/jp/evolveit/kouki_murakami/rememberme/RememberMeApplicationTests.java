867b8ece9e12c804798ed09f07046127b41bc3dc
Kouki Murakami <m.kouki.0928@gmail.com>
2022-02-17 15:34:43 +0900

    add new project.

---
+++ remember-me/src/test/java/jp/evolveit/kouki_murakami/rememberme/RememberMeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.evolveit.kouki_murakami.rememberme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
