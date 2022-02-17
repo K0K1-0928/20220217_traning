8e8a24fce261960417054495986f9e833403c3a2
Kouki Murakami <m.kouki.0928@gmail.com>
2022-02-17 18:14:45 +0900

    add Spring Security Remember-Me

---
+++ remember-me/src/main/java/jp/evolveit/kouki_murakami/rememberme/spring/MySecurityConfig.java
@@ -0,0 +1,27 @@
+package jp.evolveit.kouki_murakami.rememberme.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import java.util.Collections;
+
+@EnableWebSecurity
+@ComponentScan
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll().anyRequest()
+                .access("isAuthenticated()").and().formLogin().and().rememberMe();
+    }
+
+    @Autowired
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}password")
+                .authorities(Collections.emptyList());
+    }
+}
